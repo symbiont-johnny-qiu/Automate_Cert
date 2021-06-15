0608a0465e9305229c1804e156fb69f7dcad5829
Johnny Qiu <johnny@MacBook-Pro.local>
2021-06-15 15:18:29 -0400

    added sys module for passing in record parameters from bash script

--- Route53_add_record.py
+++ Route53_add_record.py
@@ -4,7 +4,6 @@ import sys
 client = boto3.client('route53')
 
 ## Doing change batches for creating new dns record
-
 response = client.change_resource_record_sets(
     HostedZoneId = 'Z05023012PPJJ1SFDTC0F',
     ChangeBatch = {
@@ -13,12 +12,12 @@ response = client.change_resource_record_sets(
             {
                 'Action': 'UPSERT',
                 'ResourceRecordSet': {
-                    'Name': '_37324B81E33C7AF8C2E7F48859C59F2D.ledger-jqiu-node-smoketest-today.release.symdev.us.',
+                    'Name': sys.argv[1],
                     'Type': 'CNAME',
                     'TTL': 300,
                     'ResourceRecords': [
                         {
-                            'Value': '15D6A43AFA2692E669214A2615D90602.7CD00F3B157FB7FEDFCFC9595C6EE0D6.d740de1e79667d117d8d.comodoca.com.'
+                            'Value': sys.argv[2]
                         }]
 
                 }
