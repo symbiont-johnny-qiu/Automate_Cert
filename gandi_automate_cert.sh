985093fac6fa98cd21a92644e58ab4628d318042
Johnny Qiu <johnny@MacBook-Pro.local>
2021-06-15 09:46:30 -0400

    Adding timer for Gandi to validate contact validation

--- gandi_automate_cert.sh
+++ gandi_automate_cert.sh
@@ -11,12 +11,16 @@ echo -e "\nThis is the converted CSR String : \n\n" $format_csr
 ## Parses json text to pass in the url for another PATCH command to change dcv method to dns
 echo -e "\nInitiating POST command to create new cert .........\n"
 cert_link=$(curl -X POST -H "Content-Type: application/json" -H "Authorization: Apikey spwGKaMq5UR6ZdN3nHNlsafm" "https://api.gandi.net/v5/certificate/issued-certs?sharing_id=b7d1102c-9566-11e7-9958-00163e6dc886" -d '{"package": "cert_std_1_0_0", "csr": "'"$format_csr"'"}'| jq '.href')
+## Adding a timer for Gandi to validate
+echo -e "\n Script is not finish(Do not interrupt),please wait for 5 minutes to allow Gandhi to process contact validation\n"
+sleep 5m
 echo $cert_link
 patch_cert=$(echo $cert_link| tr -d \" )"/dcv"
 echo -e "\nChanging DCV method to DNS ........\n"
 curl -X PATCH -H "Content-Type: application/json" -H "Authorization: Apikey spwGKaMq5UR6ZdN3nHNlsafm" $patch_cert -d '{"method": "dns"}'
 echo -e "\nGetting Validation Records via DNS ........\n"
-
 ## Fetch the DNS Record after it has been created
 get_dns=$(curl -X POST -H "Content-Type: application/json" -H "Authorization: Apikey spwGKaMq5UR6ZdN3nHNlsafm" https://api.gandi.net/v5/certificate/dcv_params -d '{"dcv_method": "dns", "csr": "'"$format_csr"'"}'| jq '.messages')
 echo -e "\nThe DNS Record: \n" $get_dns
+## Now pass it to AWS to automate the cert
+
