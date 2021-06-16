9b8aaa9ff587cbf724bf8102ff68369a891e6299
Johnny Qiu <johnny@macbook-pro.myfiosgateway.com>
2021-06-16 15:42:45 -0400

    Removed echo statements

--- gandi_automate_cert.sh
+++ gandi_automate_cert.sh
@@ -5,10 +5,10 @@ format_csr=$(awk 'NF {sub(/\r/, ""); printf "%s\\n",$0;}' $1)
 echo -e "\nThis is the converted CSR String : \n\n" $format_csr
 
 ## POST command to create new certificate using billing (sharing_id) and passing in csr file 
-# $(curl POST -v -H "dry-run: 1" -H "Content-Type: application/json" -H "Authorization: Apikey spwGKaMq5UR6ZdN3nHNlsafm" "https://api.gandi.net/v5/certificate/issued-certs?sharing_id=b7d1102c-9566-11e7-9958-00163e6dc886" -d '{"package": "cert_std_1_0_0", "csr": "'"$format_csr"'"}')
 ## Parses json text to pass in the url for another PATCH command to change dcv method to dns
 dry_run_check=$(curl -X POST -H "dry-run: 1" -H "Content-Type: application/json" -H "Authorization: Apikey spwGKaMq5UR6ZdN3nHNlsafm" "https://api.gandi.net/v5/certificate/issued-certs?sharing_id=b7d1102c-9566-11e7-9958-00163e6dc886" -d '{"package": "cert_std_1_0_0", "csr": "'"$format_csr"'", "dcv-method": "dns"}'| jq '.status')
 echo $dry_run_check
+
 ## Dry run just returns either Bad request or status : success, once success the curl command can proceed 
 if [ "$dry_run_check" = "\"success\"" ]; then
     cert_link=$(curl -X POST -H "Content-Type: application/json" -H "Authorization: Apikey spwGKaMq5UR6ZdN3nHNlsafm" "https://api.gandi.net/v5/certificate/issued-certs?sharing_id=b7d1102c-9566-11e7-9958-00163e6dc886" -d '{"package": "cert_std_1_0_0", "csr": "'"$format_csr"'", "dcv-method": "dns"}'| jq '.href')
@@ -26,8 +26,6 @@ if [ "$dry_run_check" = "\"success\"" ]; then
 
 ## Fetch the DNS Record after it has been created
         get_dns=$(curl -X POST -H "Content-Type: application/json" -H "Authorization: Apikey spwGKaMq5UR6ZdN3nHNlsafm" https://api.gandi.net/v5/certificate/dcv_params -d '{"dcv_method": "dns", "csr": "'"$format_csr"'"}'| jq '.messages')
-        echo -e "\nThe DNS Record: \n" $get_dns
-
 ## Now pass it to AWS to automate the cert
         get_record_name=$(echo $get_dns | awk '{print $8}')
         get_record_value=$(echo $get_dns | awk '{print $12}' | sed 's/"//')
